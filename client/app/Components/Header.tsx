42f80ec4b0d2affe5fe35f8a3c91af1840fcff3a
kennyoyaf <kehindeoyafemi@gmail.com>
2025-03-02 15:32:11 +0100

    The navbar has been completed and styles adequately

--- client/app/Components/Header.tsx
+++ client/app/Components/Header.tsx
@@ -8,15 +8,36 @@ const pages = ['About', 'Work', 'Technologies', 'Blog'];
 
 export default function Header() {
   return (
-    <AppBar position="static" sx={{ display: 'flex', background: 'red' }}>
-      <Toolbar>
+    <AppBar
+      position="static"
+      sx={{
+        display: { xs: 'none', md: 'flex' },
+        margin: '0 auto',
+        width: '90%',
+        marginTop: '20px',
+        borderRadius: '40px',
+        boxShadow: '0px 4px 30px 0px #B333E914',
+        background: '#FFFFFF'
+      }}
+    >
+      <Toolbar
+        sx={{ width: '100', display: 'flex', justifyContent: 'space-around' }}
+      >
         {pages.map(page => (
           <Link
             key={page}
             href={`/${page.toLowerCase()}`}
             style={{ textDecoration: 'none', color: 'inherit' }}
           >
-            <Typography sx={{ textAlign: 'center', cursor: 'pointer', mx: 2 }}>
+            <Typography
+              sx={{
+                color: '#3C3C3C',
+                textAlign: 'center',
+                cursor: 'pointer',
+                mx: 2
+              }}
+              variant="body1"
+            >
               {page}
             </Typography>
           </Link>
