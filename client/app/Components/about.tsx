d21a5c80c546ed6e137eb6c4bb7ebeb866f83285
kennyoyaf <kehindeoyafemi@gmail.com>
2025-03-11 14:26:50 +0100

    I have done put the typography of the about page but not styled them

--- client/app/Components/about.tsx
+++ client/app/Components/about.tsx
@@ -3,8 +3,29 @@ import React from 'react';
 
 const About = () => {
   return (
-    <Box id="about">
-      <Typography>ABOUT ME</Typography>
+    <Box
+      id="about"
+      sx={{
+        width: '100%',
+        height: '100vh',
+        background: '#E6AF2E',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+      }}
+    >
+      <Typography component="h3" variant="h3">
+        About me
+      </Typography>
+      <Typography component="p" variant="body2">
+        I am a web developer and technical writer who uses MERN stack and other
+        modern web technologies to create top-notch websites, webApps, and other
+        web solutions.
+      </Typography>
+      <Typography component="p" variant="body2">
+        I am particularly interested in start-up and small scale businesses as I
+        have a lot of experience working in this niche.
+      </Typography>
     </Box>
   );
 };
