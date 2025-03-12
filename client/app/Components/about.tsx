5d769cce07ce7672eb13686c31c6b93137beea78
kennyoyaf <kehindeoyafemi@gmail.com>
2025-03-12 02:09:06 +0100

    I have been able to get the mobile page styled but I am not yet satisfied

--- client/app/Components/about.tsx
+++ client/app/Components/about.tsx
@@ -7,37 +7,109 @@ const About = () => {
       id="about"
       sx={{
         width: '100%',
-        height: '100vh',
+        height: 'calc(100vh + 10px)',
         background: '#E6AF2E',
         display: 'flex',
         flexDirection: 'column',
-        alignItems: 'center'
+        alignItems: 'center',
+        padding: '40px 0px'
       }}
     >
-      <Typography component="h3" variant="h3">
+      <Typography component="h3" variant="h3" sx={{ paddingBottom: '30px' }}>
         About me
       </Typography>
-      <Typography component="p" variant="body2">
+      <Typography
+        component="p"
+        variant="body3"
+        sx={{ textAlign: 'center', width: '77%', paddingBottom: '20px' }}
+      >
         I am a web developer and technical writer who uses MERN stack and other
         modern web technologies to create top-notch websites, webApps, and other
         web solutions.
       </Typography>
-      <Typography component="p" variant="body2">
+      <Typography
+        component="p"
+        variant="body3"
+        sx={{ textAlign: 'center', width: '77%', paddingBottom: '60px' }}
+      >
         I am particularly interested in start-up and small scale businesses as I
         have a lot of experience working in this niche.
       </Typography>
-      <Box>
-        <Box>
-          <img src="../../Images/AboutUs-Image-1.jpg" alt="AboutUs-Image-1" />
+      <Box
+        sx={{
+          display: 'grid',
+          gridTemplateColumns: '1fr 1fr 1fr',
+          gridTemplateAreas: `
+          "a . b"
+          ". c ."
+        `,
+          width: '100%',
+          maxWidth: '100%'
+        }}
+      >
+        <Box
+          sx={{
+            width: { xs: '100px', sm: '150px', md: '200px' }, // Adjusts size by breakpoint
+            height: { xs: '100px', sm: '150px', md: '200px' },
+            gridArea: 'a',
+            borderRadius: '50%',
+            marginLeft: '50px',
+            marginTop: '-20px'
+          }}
+        >
+          <img
+            src="/Images/AboutUs-Image-1.jpg"
+            alt="AboutUs-Image-1"
+            style={{
+              width: '100%',
+              height: '100%',
+              borderRadius: '50%',
+              objectFit: 'cover'
+            }}
+          />
         </Box>
-        <Box>
-          <img src="../../Images/AboutUs-Image-2.jpg" alt="AboutUs-Image-2" />
+        <Box
+          sx={{
+            width: { xs: '100px', sm: '150px', md: '200px' }, // Adjusts size by breakpoint
+            height: { xs: '100px', sm: '150px', md: '200px' },
+            gridArea: 'c',
+            borderRadius: '50%'
+          }}
+        >
+          <img
+            src="/Images/AboutUs-Image-2.jpg"
+            alt="AboutUs-Image-2"
+            style={{
+              width: '100%',
+              height: '100%',
+              objectFit: 'cover',
+              borderRadius: '50%'
+            }}
+          />
         </Box>
-        <Box>
-          <img src="../../Images/AboutUs-Image-3.jpg" alt="AboutUs-Image-3" />
+        <Box
+          sx={{
+            width: { xs: '100px', sm: '150px', md: '200px' }, // Adjusts size by breakpoint
+            height: { xs: '100px', sm: '150px', md: '200px' },
+            gridArea: 'b',
+            borderRadius: '50%'
+          }}
+        >
+          <img
+            src="/Images/AboutUs-Image-3.jpg"
+            alt="AboutUs-Image-3"
+            style={{
+              width: '100%',
+              height: '100%',
+              borderRadius: '50%',
+              marginBottom: '-20px',
+              marginLeft: '-60px',
+              objectFit: 'cover'
+            }}
+          />
         </Box>
       </Box>
-      <Box>
+      <Box sx={{ marginTop: '100px' }}>
         <img src="../../Images/Mask group.png" alt="Click-mouse" />
       </Box>
     </Box>
