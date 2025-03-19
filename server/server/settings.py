fb4e9e8910501f19a997e65652dc025c3686bcd9
Babajide Peter Oyafemi <67855455+Babajide777@users.noreply.github.com>
2025-03-19 17:29:35 +0100

    Refactor database settings to use PostgreSQL with environment variables

--- server/server/settings.py
+++ server/server/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,10 +76,20 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': {        
+        'ENGINE': 'django.db.backends.postgresql',
+        #    'NAME': 'your_database_name',
+        #    'USER': 'your_database_user',
+        #    'PASSWORD': 'your_database_password',
+        #    'HOST': 'localhost',  
+        #    'PORT': '5432',    
+           
+           'NAME': config('DB_NAME'),
+           'USER': config('DB_USER'),
+           'PASSWORD': config('DB_PASSWORD'),
+           'HOST': config('DB_HOST'),
+           'PORT': config('DB_PORT'),
+           }
 }
 
 
