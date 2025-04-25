77ea700b97018c924a42686768c47ec421ef9af5
kennyoyaf <kehindeoyafemi@gmail.com>
2025-04-25 22:43:20 +0100

    desktop part of the framework experience box done

--- client/app/Components/technologies.tsx
+++ client/app/Components/technologies.tsx
@@ -22,7 +22,7 @@ const Technologies = () => {
         paddingBottom: '40px'
       }}>I am conversant with a range of JavaScript and TypeScript frontend, backend and CSS libraries and frameworks used in building web solutions such as React, Express, Node, BootStrap, Material UI, JQuery, and lots more.</Typography>
       <Box component='div' sx={{width: { xs: '90%', md: '80%' }, paddingBottom: '10px'}}>
-      <Box component='div' sx={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: { xs: '100%', md: '60%' }, paddingBottom: '20px'}}>
+      <Box component='div' sx={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: { xs: '100%', md: '50%' }, paddingBottom: '20px'}}>
         <Typography component="h6"
       variant="body1" sx={{fontWeight: '900',
         fontSize: '16px',textTransform:'uppercase', borderBottom:'4px solid #E6AF2E', paddingBottom:'5px', color:'#2B2B2B'}}>Frontend</Typography>
@@ -33,8 +33,8 @@ const Technologies = () => {
       variant="body1" sx={{fontWeight: '400',
         fontSize: '16px',textTransform:'uppercase', color:'#2B2B2B'}}>Technical writing</Typography>
         </Box>
-      <Box component='div' sx={{border:'1px solid #C8C8C8', borderRadius:'16px', display:'flex', flexDirection:'column',alignItems:'center', justifyContent:'center',padding:'50px'}}>
-        <Typography sx={{textAlign:'center'}}>Experience with ReactJs, VanillaJS, and JQuery</Typography>
+      <Box component='div' sx={{border:'1px solid #C8C8C8', borderRadius:'16px', display:'flex', flexDirection:{xs:'column', md:'row'}, alignItems:'center', justifyContent:{xs:'center', md:'space-between'},padding:{xs:'50px', md:'0 20px'}}}>
+        <Typography sx={{textAlign:{xs:'center',md:'left'}, width:{md:'35%', lg:'28%'}}}>Experience with ReactJs, VanillaJS, and JQuery</Typography>
         <Box component='div'>
         <img src="../../Images/boba-software-developer-workplace.png" alt="workplace-image" style={{width:'251px',height:'230px'}}/>
         </Box>
