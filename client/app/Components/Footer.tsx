52f397ab14ab74fef94b17b6039413ce55d0100c
kennyoyaf <kehindeoyafemi@gmail.com>
2025-05-13 12:56:20 +0100

    pulling code from the projects branch

--- client/app/Components/Footer.tsx
+++ client/app/Components/Footer.tsx
@@ -1,5 +1,14 @@
+import { Box, TextField, Typography } from '@mui/material';
 import React from 'react';
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return <Box>
+    <Box>
+      <Typography component='p'>
+      Do you need my services? Kindly fill the form below so we can work together.
+      </Typography>
+      <TextField id="outlined-basic" label="Enter your email address" variant="outlined" />
+    </Box>
+    <Box></Box>
+  </Box>
 }
