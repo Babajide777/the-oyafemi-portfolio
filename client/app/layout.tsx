5aa316116073a14c5dd8c6f727d9f82391c611b1
Babajide Peter Oyafemi <67855455+Babajide777@users.noreply.github.com>
2025-05-13 12:52:58 +0100

    Refactor layout.tsx for consistent formatting and import global styles

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,20 +1,21 @@
-'use client';
+"use client";
 
-import { Cormorant_SC, Outfit } from 'next/font/google';
-import { productSans, switzer } from '@/utils/fonts';
-import Layout from './Components/Layout';
+import { Cormorant_SC, Outfit } from "next/font/google";
+import { productSans, switzer } from "@/utils/fonts";
+import Layout from "./Components/Layout";
+import "./globals.css";
 
 const cormorantSC = Cormorant_SC({
-  subsets: ['latin'],
-  weight: ['400', '600', '700']
+  subsets: ["latin"],
+  weight: ["400", "600", "700"],
 });
 const outfit = Outfit({
-  subsets: ['latin'],
-  weight: ['400', '500', '700', '900']
+  subsets: ["latin"],
+  weight: ["400", "500", "700", "900"],
 });
 
 export default function RootLayout({
-  children
+  children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
