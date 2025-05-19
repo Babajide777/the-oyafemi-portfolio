6ea809d0ec7c23289d0563cdac812f27be482a7e
kennyoyaf <kehindeoyafemi@gmail.com>
2025-05-19 18:44:10 +0100

    .

--- client/app/Components/Layout.tsx
+++ client/app/Components/Layout.tsx
@@ -1,32 +1,22 @@
-import { Container, CssBaseline, ThemeProvider } from '@mui/material';
+'use client';
+
+import { Box, Container, CssBaseline, ThemeProvider } from '@mui/material';
 import Head from 'next/head';
 import React from 'react';
 import { LayoutProps } from '@/utils/type';
 import getTheme from '@/utils/styles';
-import Script from 'next/script';
 
 export default function Layout({ title, children, darkMode }: LayoutProps) {
   return (
     <ThemeProvider theme={getTheme({ darkMode })}>
+      <CssBaseline />
       <Head>
-        {/* This script runs before React hydration */}
-        <Script
-          id="strip-extension-attrs"
-          strategy="beforeInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              // Remove attributes injected by known extensions
-              document.documentElement.removeAttribute('crosspilot');
-              document.documentElement.removeAttribute('cz-shortcut-listen');
-            `,
-          }}
-        />
         <title>
           {title ? `${title} - Oyafemi Portfolio` : 'Oyafemi Portfolio'}
         </title>
       </Head>
-      <CssBaseline />
-      <Container>{children}</Container>
+      <Box component="main">{children}</Box>
     </ThemeProvider>
   );
 }
+1;
