39311e04cab5dddaf4a3c583d5dd802fcfea788c
kennyoyaf <kehindeoyafemi@gmail.com>
2025-06-30 20:01:32 +0100

    Added some more colors to the style.js

--- admin/utils/styles.ts
+++ admin/utils/styles.ts
@@ -11,11 +11,16 @@ declare module "@mui/material/styles" {
       Black100: string;
       Black200: string;
       Black300: string;
+      Black400: string;
       lightGray: string;
+      lightGray2: string;
+      lightGray3: string;
       darkGray: string;
       darkGray2: string;
       gray100: string;
       gray200: string;
+      gray300: string;
+      coolGray: string;
     };
   }
   interface PaletteOptions {
@@ -25,11 +30,16 @@ declare module "@mui/material/styles" {
       Black100?: string;
       Black200?: string;
       Black300?: string;
+      Black400?: string;
       lightGray?: string;
+      lightGray2?: string;
+      lightGray3?: string;
       darkGray?: string;
       darkGray2?: string;
       gray100?: string;
       gray200?: string;
+      gray300?: string;
+      coolGray?: string;
     };
   }
 }
@@ -53,11 +63,16 @@ const getTheme = ({ darkMode }: GetThemeOptions): Theme =>
         Black100: "#000000",
         Black200: "#1E1E1E",
         Black300: "#18181B",
+        Black400: "#0B0B0B",
         gray100: "#C1C1C1",
         gray200: "#D4D4D8",
+        gray300: "#B6B6B6",
         lightGray: "#EFEFEF",
+        lightGray2: "#DBDBDB",
+        lightGray3: "#E4E4E7",
         darkGray: "#52525B",
         darkGray2: "#FBFBFB",
+        coolGray: "#111827",
       },
     },
   });
